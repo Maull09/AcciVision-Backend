bc5cbef3fa0427d4b84c05f823e573d1815a3399
Maull09 <maulanafirdaus63@gmail.com>
2024-12-20 01:31:45 +0700

    fix: change opencv to pillow

--- detection.py
+++ detection.py
@@ -1,17 +1,18 @@
-import cv2
+from PIL import Image
+from io import BytesIO
 import numpy as np
 from ultralytics import YOLO
 
 def detect_objects(image_bytes):
-    # Convert bytes to numpy array
-    np_array = np.frombuffer(image_bytes, np.uint8)
-    image = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+    # Convert bytes to a Pillow image
+    image = Image.open(BytesIO(image_bytes))
+    image_np = np.array(image)
 
     # Load YOLO model
     model = YOLO("best.pt")  # Replace with your YOLOv8 model
 
     # Perform inference
-    results = model(image)
+    results = model(image_np)
 
     # Parse results
     detections = []
